fd5a68c9200ef68dc0740ffb878dbff15c1c0cba
Iimp101 <magnuslundberg86@live.com>
2025-05-19 15:50:11 +0200

    Added fetch quests for my starwars encyclopedia

--- fed24-js2-uppgift-1-Iimp101/src/services/StarwarsPediaAPI.ts
+++ fed24-js2-uppgift-1-Iimp101/src/services/StarwarsPediaAPI.ts
@@ -1,4 +1,9 @@
 import axios from "axios";
+import { Person, 
+    Film, Planet, 
+    Species, Starship, 
+    Vehicle, 
+    SWAPIListResponse } from "./StarwarsPedia.TYPES";
 
 const instance = axios.create({
     baseURL: "https://swapi.thehiveresistance.com/api",
@@ -9,7 +14,57 @@ const instance = axios.create({
     },
 });
 
-const get = async <T>(endpoint: string) => {
-    const res = instance.get<T>(endpoint);
-    return res.data;
-}
+const get = async <T>(endpoint: string): Promise<T> => {
+	const res = await instance.get<T>(endpoint);
+	return res.data;
+};
+
+export const getList = async <T>(resource: string, page = 1, searchQuery = "") => {
+	let endpoint = `/${resource}/?page=${page}`;
+	if (searchQuery.trim()) {
+		endpoint += `&search=${searchQuery}`;
+	}
+	return get<SWAPIListResponse<T>>(endpoint);
+};
+
+// People(s)
+export const getPersonList = (page?: number, search?: string) => 
+    getList<Person>("people", page, search);
+
+export const getPersonById = (id: number) =>
+	get<Person>(`/people/${id}`);
+
+// Film(s)
+export const getFilmList = (page?: number, search?: string) =>
+	getList<Film>("films", page, search);
+
+export const getFilmById = (id: number) =>
+	get<Film>(`/films/${id}`);
+
+// Planet(s)
+export const getPlanetList = (page?: number, search?: string) =>
+	getList<Planet>("planets", page, search);
+
+export const getPlanetById = (id: number) =>
+	get<Planet>(`/planets/${id}`);
+
+// Specie(s)
+export const getSpeciesList = (page?: number, search?: string) =>
+	getList<Species>("species", page, search);
+
+export const getSpeciesById = (id: number) =>
+	get<Species>(`/species/${id}`);
+
+// Starship(s)
+export const getStarshipList = (page?: number, search?: string) =>
+	getList<Starship>("starships", page, search);
+
+export const getStarshipById = (id: number) =>
+	get<Starship>(`/starships/${id}`);
+
+// Vehicle(s)
+export const getVehicleList = (page?: number, search?: string) =>
+	getList<Vehicle>("vehicles", page, search);
+
+export const getVehicleById = (id: number) =>
+	get<Vehicle>(`/vehicles/${id}`);
