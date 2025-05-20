42f736c77ce10ac07e280a2920177e6563f09d23
Iimp101 <magnuslundberg86@live.com>
2025-05-20 22:10:31 +0200

    fixed an css bug. Removed index.css and import from main.tsx. I added styling to html, body in app.css instead

--- fed24-js2-uppgift-1-Iimp101/src/App.tsx
+++ fed24-js2-uppgift-1-Iimp101/src/App.tsx
@@ -1,31 +1,30 @@
-
-import { Route, Routes } from 'react-router-dom'
-import Navigation from './components/Navigation';
-import Container from 'react-bootstrap/Container';
-import HomePage from './pages/HomePage';
-import PeoplesPages from './pages/PeoplesPage';
-import './App.css'
-import PlanetsPages from './pages/PlanetsPage';
-import SpeciesPage from './pages/SpeciesPage';
-import StarshipsPage from './pages/StarshipsPage';
-import VehiclesPage from './pages/VehiclesPage';
+import { Route, Routes } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import HomePage from "./pages/HomePage";
+import PeoplesPages from "./pages/PeoplesPage";
+import PlanetsPages from "./pages/PlanetsPage";
+import SpeciesPage from "./pages/SpeciesPage";
+import StarshipsPage from "./pages/StarshipsPage";
+import VehiclesPage from "./pages/VehiclesPage";
+import "./App.css";
 
 function App() {
 	return (
-    	<>
+		<>
 			<Navigation />
-			<Container className="my-4">
+
+			<div>
 				<Routes>
 					<Route path="/" element={<HomePage />} />
 					<Route path="/people" element={<PeoplesPages />} />
 					<Route path="/planets" element={<PlanetsPages />} />
-					<Route path="/species" element={<SpeciesPage/>} />
+					<Route path="/species" element={<SpeciesPage />} />
 					<Route path="/starships" element={<StarshipsPage />} />
 					<Route path="/vehicles" element={<VehiclesPage />} />
 				</Routes>
-			</Container>
-    	</>
-  	)
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
