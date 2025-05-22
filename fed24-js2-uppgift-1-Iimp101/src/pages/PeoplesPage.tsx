0e1b945d9bb447cb45727231b0b9091293fbca7e
Iimp101 <magnuslundberg86@live.com>
2025-05-22 17:46:08 +0200

    created a map data/PeopleImages and exported it to PeoplePage.tsx

--- fed24-js2-uppgift-1-Iimp101/src/pages/PeoplesPage.tsx
+++ fed24-js2-uppgift-1-Iimp101/src/pages/PeoplesPage.tsx
@@ -4,28 +4,8 @@ import { getPersonList } from "../services/StarwarsPediaAPI";
 import type { Person, SWAPIListResponse } from "../services/StarwarsPedia.types";
 import Pagination from "../components/Pagination";
 import LoadingPagesGif from "../components/LoadingPagesGif";
+import missingImages from "../data/PeopleImages";
 import "../CSS/PeoplesPage.css";
-
-	const missingImages: Record<number, string> = {
-		37: "/images/PeopleImages/Roos Tarpals.png",
-		38: "/images/PeopleImages/Rugor Nass.png",
-		43: "/images/PeopleImages/Shmi Skywalker.png",
-		47: "/images/PeopleImages/Ratts Tyerell.png",
-		49: "/images/PeopleImages/Gasgano.png",
-		50: "/images/PeopleImages/Ben Quadinaros.png",
-		51: "/images/PeopleImages/Mace Windu.png",
-		55: "/images/PeopleImages/Adi Gallia.png",
-		56: "/images/PeopleImages/Saesee Tiin.png",
-		57: "/images/PeopleImages/Yarael Poof.png",
-		61: "/images/PeopleImages/Cordé.png",
-		64: "/images/PeopleImages/Luminara Unduli.png",
-		66: "/images/PeopleImages/Dormé.png",
-		71: "/images/PeopleImages/Dexter Jettster.png",
-		77: "/images/PeopleImages/San Hill.png",
-		79: "/images/PeopleImages/Grievous.png",
-		82: "/images/PeopleImages/Sly Moore.png",
-		83: "/images/PeopleImages/Tion Medon.png",
-};
 
 const PeoplesPage = () => {
 	const [people, setPeople] = useState<Person[]>([]);
